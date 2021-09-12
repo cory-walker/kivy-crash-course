36e3636cd193fbb2c5a16cdddb7f549ce4c5f76e
cory walker <cory-walker@live.com>
2021-09-11 21:03:51 -0400

    added lessons 5 to 8. 8 is incomplete

--- 6-kivy-properties.py
+++ 6-kivy-properties.py
@@ -5,16 +5,35 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ListProperty
+
 import random
 
-# tutorial video: https://www.youtube.com/watch?v=ZmteLworB4E&list=PLdNh1e1kmiPP4YApJm8ENK2yMlwF1_edq&index=5
+# tutorial video: https://www.youtube.com/watch?v=OkW-1uzP5Og&list=PLdNh1e1kmiPP4YApJm8ENK2yMlwF1_edq&index=6
 
 
 class ScatterTextWidget(BoxLayout):
+
+    text_colour = ListProperty([1, 0, 0, 1])
+
     def change_label_colour(self, *args):
         colour = [random.random() for i in range(3)] + [1]
+        self.text_colour = colour
+
+        ''' # changing colour by ID method
         label = self.ids['my_label']
         label.color = colour
+
+        label1 = self.ids.label1
+        label2 = self.ids['label2']
+
+        label1.color = colour
+        label2.color = colour
+        '''
+
+    def on_text_colour(self, *args):
+        pass
 
 
 class TutorialApp(App):
---
+++ 8-troubleshooting.py
@@ -0,0 +1,49 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ListProperty
+
+from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Line)
+
+from kivy.graphics.context_instructions import Color
+
+import random
+
+# tutorial video: https://www.youtube.com/watch?v=2Gc8iYJQ_qk&list=PLdNh1e1kmiPP4YApJm8ENK2yMlwF1_edq&index=8
+
+
+class ScatterTextWidget(BoxLayout):
+
+    text_colour = ListProperty([1, 0, 0, 1])
+
+    def __init__(self, **kwargs):
+        super(ScatterTextWidget, self).__init__(**kwargs)
+
+        # with self.canvas.before:
+
+        with self.canvas:
+            Color(0, 1, 0, 1)
+            self.rectangle = Rectangle(pos=(0, 100), size=(300, 100))
+            Ellipse(pos=(0, 400), size=(300, 100))
+            Line(points=[0, 0, 500, 600, 400, 300], close=True, width=3)
+
+    def on_size(self, *args):
+        self.rectangle.size = self.size
+
+    def change_label_colour(self, *args):
+        colour = [random.random() for i in range(3)] + [1]
+        self.text_colour = colour
+
+
+class TutorialApp(App):
+    def build(self):
+        return ScatterTextWidget()
+
+
+if __name__ == "__main__":
+    TutorialApp().run()
