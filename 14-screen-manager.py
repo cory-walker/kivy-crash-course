da225061052ea42d25e3b977497f927c725275e0
cory walker <cory-walker@live.com>
2021-09-13 21:00:57 -0400

    completed lesson 14

--- 14-screen-manager.py
+++ 14-screen-manager.py
@@ -1,70 +1,110 @@
 from kivy.app import App
+from kivy.base import runTouchApp
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-
 from kivy.properties import ListProperty
+from kivy.uix.boxlayout import BoxLayout
 
-# Tutorial: https://www.youtube.com/watch?v=xx-NLOg6x8o
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+import time
+import random
 
 
-class ColourScreen(BoxLayout):
-    colour = ListProperty([1., 0., 0., 1.)
+class FirstScreen(Screen):
+    pass
 
-root_widget= Builder.load_string('''
-BoxLayout:
-    orientation: 'vertical'
+
+class SecondScreen(Screen):
+    pass
+
+
+class ColourScreen(Screen):
+    colour = ListProperty([1., 0., 0., 1.])
+
+
+class MyScreenManager(ScreenManager):
+    def new_colour_screen(self):
+        name = str(time.time())
+        s = ColourScreen(name=name,
+                         colour=[random.random() for _ in range(3)] + [1])
+        self.add_widget(s)
+        self.current = name
+
+
+root_widget = Builder.load_string('''
+#:import FadeTransition kivy.uix.screenmanager.FadeTransition
+MyScreenManager:
+    transition: FadeTransition()
     FirstScreen:
     SecondScreen:
-    ColourScreen:
-
 <FirstScreen>:
-    orientation: 'vertical'
-    Label:
-        text: 'first screen'
-        font_size: 30
-    Image:
-        source: 'colours.png'
-        allow_strech: True
-        keep_ratio: False
+    name: 'first'
     BoxLayout:
-        Button:
-            text: 'goto second screen'
+        orientation: 'vertical'
+        Label:
+            text: 'first screen!'
             font_size: 30
-        Button:
-            text: 'get random colour screen'
-            font_size: 30
+        Image:
+            source: 'colours.png'
+            allow_stretch: True
+            keep_ratio: False
+        BoxLayout:
+            Button:
+                text: 'goto second screen'
+                font_size: 30
+                on_release: app.root.current = 'second'
+            Button:
+                text: 'get random colour screen'
+                font_size: 30
+                on_release: app.root.new_colour_screen()
 <SecondScreen>:
-    orientation: 'vertical'
-    Label:
-        text: 'second screen'
-        font_size: 30
-    Image:
-        source: 'colours.png'
-        allow_strech: True
-        keep_ratio: False
+    name: 'second'
     BoxLayout:
-        Button:
-            text: 'goto second screen'
+        orientation: 'vertical'
+        Label:
+            text: 'second screen!'
             font_size: 30
-        Button:
-            text: 'get random colour screen'
-            font_size: 30
+        Image:
+            source: 'colours2.png'
+            allow_stretch: True
+            keep_ratio: False
+        BoxLayout:
+            Button:
+                text: 'goto first screen'
+                font_size: 30
+                on_release: app.root.current = 'first'
+            Button:
+                text: 'get random colour screen'
+                font_size: 30
+                on_release: app.root.new_colour_screen()
 <ColourScreen>:
-    orientation: 'vertical'
-    Label:
-        text: 'first screen'
-        font_size: 30
-    Image:
-        source: 'colours.png'
-        allow_strech: True
-        keep_ratio: False
     BoxLayout:
-        Button:
-            text: 'goto second screen'
+        orientation: 'vertical'
+        Label:
+            text: 'colour {:.2},{:.2},{:.2} screen'.format(*root.colour[:3])
             font_size: 30
-        Button:
-            text: 'get random colour screen'
-            font_size: 30
+        Widget:
+            canvas:
+                Color:
+                    rgba: root.colour
+                Ellipse:
+                    pos: self.pos
+                    size: self.size
+        BoxLayout:
+            Button:
+                text: 'goto first screen'
+                font_size: 30
+                on_release: app.root.current = 'first'
+            Button:
+                text: 'get random colour screen'
+                font_size: 30
+                on_release: app.root.new_colour_screen()
 ''')
+
+
+class ScreenManagerApp(App):
+    def build(self):
+        return root_widget
+
+
+ScreenManagerApp().run()
