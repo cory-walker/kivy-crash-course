9255b57600d781184104961f70b6a5fa4677c0ac
cory walker <cory-walker@live.com>
2021-09-29 21:47:16 -0400

    FINALLY got popups to update global properties

--- 17-popups.py
+++ 17-popups.py
@@ -1,78 +1,49 @@
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.base import runTouchApp
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.app import App
+from kivy.uix.popup import Popup
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+Builder.load_string('''
+<SimpleButton>:
+    on_press: self.fire_popup()
+<SimplePopup>:
+    id:pop
+    size_hint: .4, .4
+    auto_dismiss: False
+    title: 'Hello world!!'
+    Button:
+        text: 'Click here to dismiss'
+        on_press: pop.chosen_rarity="common";pop.set_and_dismiss()
+''')
+
+rarity = "?"
 
 
-class Rarity_popup(Popup):
-    pass
-
-
-class Scrn1(Screen):
-    pass
-
-
-class Mgr(ScreenManager):
-    rarity_selection = "?"
+class SimplePopup(Popup):
+    chosen_rarity = StringProperty("?")
 
     def __init__(self, **kwargs):
-        super(Mgr, self).__init__(**kwargs)
+        super(SimplePopup, self).__init__(**kwargs)
+
+    def set_and_dismiss(self, *args):
+        global rarity
+        rarity = self.chosen_rarity
+        print(rarity)
+        self.dismiss()
 
 
-# popup = Popup(title='test popup', content=Label(
-#    text='Hello world'), size_hint=(None, None), size=(400, 400))
+class SimpleButton(Button):
+    text = "Fire Popup !"
+
+    def fire_popup(self):
+        pops = SimplePopup()
+        pops.open()
 
 
-root_widget = Builder.load_string('''
-#:import FadeTransition kivy.uix.screenmanager.FadeTransition
-#:import Factory kivy.factory.Factory
-Mgr:
-    transition: FadeTransition()
-    id: manager
-    Scrn1:
-
-<Rarity_popup>:
-    title: "Rarities"
-    id: rar_pop
-    BoxLayout:
-        orientation: 'vertical'
-        Button:
-            text: "common"
-            on_release: root.rarity_selection="common"; rar_pop.dismiss(); ThisApp.set_rarity("common")
-        Button:
-            text: "uncommon"
-            on_release: root.rarity_selection="uncommon"; rar_pop.dismiss()
-        Button:
-            text: "rare"
-            on_release: root.rarity_selection="rare"; rar_pop.dismiss()
-        Button:
-            text: "very rare"
-            on_release: root.rarity_selection="very rare"; rar_pop.dismiss()
-        Button:
-            text: "legendary"
-            on_release: root.rarity_selection="legendary"; rar_pop.dismiss()
-
-<Scrn1>:
-    Button:
-        id: rarity_btn
-        text: 'Rarity: ?'
-        on_release: Factory.Rarity_popup().open()
-
-'''
-                                  )
-
-
-class ThisApp(App):
-    rarity_selection = "?"
-
+class SampleApp(App):
     def build(self):
-        return root_widget
-
-    def set_rarity(self, rarity, *args):
-        self.rarity_selection = rarity
+        return SimpleButton()
 
 
-ThisApp().run()
+SampleApp().run()
